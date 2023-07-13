520a25fec13b1ba93317bce6cda015cb9e47a7bb
arshia <arshiasaleem98@gmail.com>
2023-07-13 14:41:55 +0100

    Remove ignored files

--- jest.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from '@jest/types';
-
-const config: Config.InitialOptions = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  // Add any additional configuration options you need
-};
-
-export default config;
